bc0324d1b8dd6f3ace6639431985e1c5dc1e55fa
haogre <haogre@gmail.com>
2016-10-25 15:01:45 +0800

    plan to be a high level one

--- src/leetcode/Demo.java
+++ src/leetcode/Demo.java
@@ -1,5 +1,14 @@
 package leetcode;
 
+/**
+ * @author Administrator
+ * @description 
+ * @date 2016Äê10ÔÂ25ÈÕ
+ */
 public class Demo {
-
+	
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+	
 }
