9e27e932c5f381790c06fc3389e5bf9597972d6d
haogre <haogre@gmail.com>
2016-10-28 14:58:10 +0800

    add problem2

--- src/leetcode/Problem002Solution.java
+++ src/leetcode/Problem002Solution.java
@@ -4,7 +4,7 @@ import leetcode.extra.ListNode;
 
 /**
  * @author haogre
- * @description
+ * @description /10 进位
  * @date 2016年10月26日
  */
 public class Problem002Solution {
@@ -52,6 +52,32 @@ public class Problem002Solution {
             point.next = new ListNode(carry);
         }
         return head.next;
+    }
+    
+    /**
+     * official Solution
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbersOffical(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
 }
