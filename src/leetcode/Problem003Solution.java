19b7563fc3a73af9677ab6264f9586ccc5044176
haogre <haogre@gmail.com>
2016-10-31 14:25:47 +0800

    3 methods abandon my fuck method and analyze another two methods

--- src/leetcode/Problem003Solution.java
+++ src/leetcode/Problem003Solution.java
@@ -1,7 +1,7 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author haogre
@@ -11,59 +11,75 @@ import java.util.List;
 public class Problem003Solution {
 
 	/**
-	 * my Solution
-	 * 
+	 * my Solution 
 	 * @param s
 	 * @return
 	 */
-	public int lengthOfLongestSubstring(String s) {
-
-		List<String> resultStr = new ArrayList<>();
-
-		String result = "";
-		int length = 0;
-		if (!"".equals(s)) {
-			for (int j = 0; j < s.length(); j++) {
-				for (int i = 0; i < s.length(); i++) {
-					if ("".equals(result)) {
-						result = result + s.substring(i, i + 1);
-					} else {
-						if (result.contains(s.substring(i, i + 1))) {
-							resultStr.add(result);
-							result = "";
-						} else {
-							result = result + s.substring(i, i + 1);
-							if (i == s.length() - 1) {
-								resultStr.add(result);
-							}
-						}
-					}
-				}
-			}
-			if (resultStr.size() > 0) {
-				for (String string : resultStr) {
-					if (string.length() > length) {
-						length = string.length();
-					}
-				}
-			} else {
-				length = 0;
-			}
-
-		} else {
-			length = 0;
-		}
-		return length;
+	public int lengthOfLongestSubstringMySolution(String s) {
+		//套了N此循环通过了测试，就不再贴出来了，直接上官方解题思路
+		return 0;
 	}
+	
+    /**
+     * 但是实际上，对于这个题目，不需要用hashmap，因为所有的字符ASCII码加起来也就最多255个，可以直接用数组来代替hashmap，效率更高。
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstringOffical(String s) {
+        int[] map = new int[256]; // map from character's ASCII to its last occured index
+        
+        int j = 0;
+        int i = 0;
+        int ans = 0;
+        for (i = 0; i < s.length(); i++) {
+            while (j < s.length() && map[s.charAt(j)]==0) {
+                map[s.charAt(j)] = 1;
+                ans = Math.max(ans, j-i + 1);
+                j ++;
+            }
+            map[s.charAt(i)] = 0;
+        }
+        
+        return ans;
+    }
+    
+    /**
+     * Offical Solution
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
 
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+    
+    
 	public static void main(String[] args) {
 		String s = "abcabcbb";
 		String s1 = "bbbbb";
 		String s2 = "pwwkew";
+		String s3 = "c";
+		String s4 = "ohomm";
+		System.out.println(s4.charAt(2));
 
 		Problem003Solution solution = new Problem003Solution();
-		System.out.println(solution.lengthOfLongestSubstring(s));
-		System.out.println(solution.lengthOfLongestSubstring(s1));
-		System.out.println(solution.lengthOfLongestSubstring(s2));
+//		System.out.println(solution.lengthOfLongestSubstring(s));
+//		System.out.println(solution.lengthOfLongestSubstring(s1));
+//		System.out.println(solution.lengthOfLongestSubstring(s2));
+		System.out.println(solution.lengthOfLongestSubstringOffical(s4));
 	}
 }
