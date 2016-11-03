76beb3139e498f0bbbddf7ae61e1e355f930546e
haogre <haogre@gmail.com>
2016-11-03 15:21:22 +0800

    此题并未解出 情况太多考虑不到如何全部取出不符合的情况 直接上官方答案

---
+++ src/leetcode/Problem008Solution.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+/**
+ * @author Administrator
+ * @description String to Integer (atoi  //此题并未解出 情况太多考虑不到如何全部取出不符合的情况
+ * @date 2016年11月3日
+ */
+public class Problem008Solution {
+	public int myAtoi(String str) {
+		if(str == null) {
+            return 0;
+        }
+        str = str.trim();
+        if (str.length() == 0) {
+            return 0;
+        }
+            
+        int sign = 1;
+        int index = 0;
+    
+        if (str.charAt(index) == '+') {
+            index++;
+        } else if (str.charAt(index) == '-') {
+            sign = -1;
+            index++;
+        }
+        long num = 0;
+        for (; index < str.length(); index++) {
+            if (str.charAt(index) < '0' || str.charAt(index) > '9')
+                break;
+            num = num * 10 + (str.charAt(index) - '0');
+            if (num > Integer.MAX_VALUE ) {
+                break;
+            }
+        }   
+        if (num * sign >= Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (num * sign <= Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int)num * sign;
+	}
+
+	public static void main(String[] args) {
+		Problem008Solution s008 = new Problem008Solution();
+		System.out.println(s008.myAtoi("2147483648"));
+	}
+}
