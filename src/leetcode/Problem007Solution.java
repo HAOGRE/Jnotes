7424ffad00acd04591a09f001ea8925c117c3ce2
haogre <haogre@gmail.com>
2016-12-08 17:07:10 +0800

    s007 s

--- src/leetcode/Problem007Solution.java
+++ src/leetcode/Problem007Solution.java
@@ -4,5 +4,28 @@ package leetcode;
  * Created by haogre on 12/6/2016.
  */
 public class Problem007Solution {
-	
+	public int reverse(int x) {
+
+		if (x == 0)return x;
+
+        int reversed = 0;
+        while (x != 0) {
+            int temp = reversed * 10 + x % 10;
+            x = x / 10;
+            if (temp / 10 != reversed) {
+                reversed = 0;
+                break;
+            }
+            reversed = temp;
+        }
+        return reversed;
+    }
+
+
+	public static void main(String[] args) {
+		Problem007Solution s7 = new Problem007Solution();
+		int x = 123;
+		System.out.println(x/10/10);
+		System.out.println(s7.reverse(x));
+	}
 }
