c5e864e8ad0f1d2d01d5fe511f17482e39ca11b2
hao.zhang <hao.zhang@beebank.com>
2017-02-07 15:53:10 +0800

    21

---
+++ src/leetcode/Problem021Solution.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+
+import leetcode.extra.ListNode;
+
+/**
+ * Created by haozhang on 2017/2/7.
+ */
+public class Problem021Solution {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+
+        return null;
+    }
+}
+
+
