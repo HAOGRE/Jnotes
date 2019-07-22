77895201f8ae4c8819b8865b0af5a3f6cbd29e61
haogre <haogre@gmail.com>
2019-07-22 15:55:48 +0800

    singleton over

--- src/haogre/dp/singleton/TestSingleton.java
+++ src/haogre/dp/singleton/TestSingleton.java
@@ -1,12 +1,6 @@
 package haogre.dp.singleton;
 
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 /**
  * Created with IntelliJ IDEA
  *
