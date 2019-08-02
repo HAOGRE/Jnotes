51b27c7885cb8f6fc53f3c5e6b2e7c8f57d47721
haogre <haogre@gmail.com>
2019-08-02 14:30:42 +0800

    s

---
+++ src/com/haogre/java8/lambda/Samples2.java
@@ -0,0 +1,13 @@
+package com.haogre.java8.lambda;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Project : leetcode
+ * @Description: Samples2
+ * @Author : haogre@gmail.com
+ * @Date : 2019-07-31 13:42
+ * @Version : V1.0
+ **/
+public class Samples2 {
+}
