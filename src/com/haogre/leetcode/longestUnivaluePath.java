2d6ad1540141916ce0c636c29f5d8b4c7c7ea9b7
haogre <haogre@gmail.com>
2020-03-06 13:35:15 +0800

    timeline bugfix

---
+++ src/com/haogre/leetcode/longestUnivaluePath.java
@@ -0,0 +1,13 @@
+package com.haogre.leetcode;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Project : Jnotes
+ * @Description:
+ * @Author : haogre@gmail.com
+ * @Date : 3/6/20 10:40 AM
+ * @Version : V1.0
+ **/
+public class longestUnivaluePath {
+}
