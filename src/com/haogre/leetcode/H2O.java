cdf32da6ca13b0f2952172610eda1a2de63f17b6
haogre <haogre@gmail.com>
2020-03-11 14:20:22 +0800

    thread waiting

--- src/com/haogre/leetcode/H2O.java
+++ src/com/haogre/leetcode/H2O.java
@@ -1,5 +1,9 @@
 package com.haogre.leetcode;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 /**
  * Created with IntelliJ IDEA
  *
@@ -9,5 +13,44 @@ package com.haogre.leetcode;
  * @Date : 3/6/20 1:44 PM
  * @Version : V1.0
  **/
-public class H2O {
+class H2O {
+
+    private CyclicBarrier barrier = new CyclicBarrier(3, new Runnable() {
+        @Override
+        public void run() {
+            semaphoreH.release(2);
+            semaphoreO.release();
+        }
+    });
+
+    private Semaphore semaphoreH;
+    private Semaphore semaphoreO;
+
+    public H2O() {
+        semaphoreH = new Semaphore(2);
+        semaphoreO = new Semaphore(1);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        semaphoreH.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        try {
+            barrier.await();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        semaphoreO.acquire();
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        try {
+            barrier.await();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
