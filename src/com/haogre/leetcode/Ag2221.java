41df7ccf8d363a1340268dc4d2dfc5399ec1e4ba
haogre <haogre@gmail.com>
2020-05-08 16:59:33 +0800

    Ag2221-动态规划

--- src/com/haogre/leetcode/Ag2221.java
+++ src/com/haogre/leetcode/Ag2221.java
@@ -83,7 +83,14 @@ public class Ag2221 {
                     if (i == 0 || j == 0) {
                         dp[i][j] = 1;
                     } else {
-                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                        dp[i][j] = Math.min(
+                                Math.min(
+                                        // 左边
+                                        dp[i - 1][j],
+                                        // 上边
+                                        dp[i][j - 1]),
+                                        // 对角
+                                        dp[i - 1][j - 1]) + 1;
                     }
                     maxSide = Math.max(maxSide, dp[i][j]);
                 }
