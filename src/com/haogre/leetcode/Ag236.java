4bc59bb0546f43898733314132167d83156a2d9a
haogre <haogre@gmail.com>
2020-05-11 15:50:34 +0800

    Ag236.java

--- src/com/haogre/leetcode/Ag236.java
+++ src/com/haogre/leetcode/Ag236.java
@@ -1,5 +1,7 @@
 package com.haogre.leetcode;
 
+import javax.swing.tree.TreeNode;
+
 /**
  * Created with IntelliJ IDEA
  *
@@ -10,4 +12,24 @@ package com.haogre.leetcode;
  * @Version : V1.0
  **/
 public class Ag236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // LCA 问题
+        if (root == null) {
+            return root;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        }
+        return null;
+    }
 }
