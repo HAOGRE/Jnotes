66ae2a6c918bb9f297e10c820518dc577d9eae45
haogre <haogre@gmail.com>
2020-05-12 16:08:34 +0800

    Ag416.java

---
+++ src/com/haogre/leetcode/Ag416.java
@@ -0,0 +1,62 @@
+package com.haogre.leetcode;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Project : Jnotes
+ * @Description: https://leetcode-cn.com/problems/partition-equal-subset-sum/
+ * @Author : haogre@gmail.com
+ * @Date : 2020/5/12 10:52
+ * @Version : V1.0
+ * 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ *
+ * 注意:
+ *
+ * 每个数组中的元素不会超过 100
+ * 数组的大小不会超过 200
+ *
+ **/
+public class Ag416 {
+
+    public boolean canPartition(int[] nums) {
+        int len = nums.length;
+        // 1. 求和 奇数不可分割
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        // 目标数组的值得总计
+        int target = sum / 2;
+
+        // 创建二维状态数组，行：物品索引，列：容量（包括 0）
+        boolean[][] dp = new boolean[len][target + 1];
+
+        // 先填表格第 0 行，第 1 个数只能让容积为它自己的背包恰好装满
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+
+        // 再填表格后面几行
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j <= target; j++) {
+                // 直接从上一行先把结果抄下来，然后再修正
+                dp[i][j] = dp[i - 1][j];
+
+                if (nums[i] == j) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                if (nums[i] < j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+        return dp[len - 1][target];
+    }
+
+}
