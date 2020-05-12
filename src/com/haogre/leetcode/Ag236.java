9db9c7026427f5622b03f52c66ac46c72b3a49c0
haogre <haogre@gmail.com>
2020-05-12 16:08:26 +0800

    Ag236.java

--- src/com/haogre/leetcode/Ag236.java
+++ src/com/haogre/leetcode/Ag236.java
@@ -13,23 +13,23 @@ import javax.swing.tree.TreeNode;
  **/
 public class Ag236 {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // LCA 问题
-        if (root == null) {
-            return root;
-        }
-        if (root == p || root == q) {
-            return root;
-        }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) {
-            return root;
-        } else if (left != null) {
-            return left;
-        } else if (right != null) {
-            return right;
-        }
-        return null;
-    }
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        // LCA 问题
+//        if (root == null) {
+//            return root;
+//        }
+//        if (root == p || root == q) {
+//            return root;
+//        }
+//        TreeNode left = lowestCommonAncestor(root.left, p, q);
+//        TreeNode right = lowestCommonAncestor(root.right, p, q);
+//        if (left != null && right != null) {
+//            return root;
+//        } else if (left != null) {
+//            return left;
+//        } else if (right != null) {
+//            return right;
+//        }
+//        return null;
+//    }
 }
