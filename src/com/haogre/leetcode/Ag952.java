151537e8823d7717afca85660db48d65f93526e6
haogre <haogre@gmail.com>
2020-05-15 16:30:22 +0800

    Ag952.java

---
+++ src/com/haogre/leetcode/Ag952.java
@@ -0,0 +1,13 @@
+package com.haogre.leetcode;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Project : Jnotes
+ * @Description: https://leetcode-cn.com/problems/largest-component-size-by-common-factor/
+ * @Author : haogre@gmail.com
+ * @Date : 2020/5/15 16:17
+ * @Version : V1.0
+ **/
+public class Ag952 {
+}
