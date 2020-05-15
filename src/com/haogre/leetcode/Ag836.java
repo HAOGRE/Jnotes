b940a2e13c398a159050804e20a08539012976bf
haogre <haogre@gmail.com>
2020-05-15 15:46:58 +0800

    Ag440

---
+++ src/com/haogre/leetcode/Ag836.java
@@ -0,0 +1,52 @@
+package com.haogre.leetcode;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Project : Jnotes
+ * @Description: https://leetcode-cn.com/problems/rectangle-overlap/
+ * @Author : haogre@gmail.com
+ * @Date : 2020/5/15 15:42
+ * @Version : V1.0
+ * 矩形以列表 [x1, y1, x2, y2] 的形式表示，其中 (x1, y1) 为左下角的坐标，(x2, y2) 是右上角的坐标。
+ *
+ * 如果相交的面积为正，则称两矩形重叠。需要明确的是，只在角或边接触的两个矩形不构成重叠。
+ *
+ * 给出两个矩形，判断它们是否重叠并返回结果。
+ *
+ * 矩形以列表 [x1, y1, x2, y2] 的形式表示，其中 (x1, y1) 为左下角的坐标，(x2, y2) 是右上角的坐标。
+ *
+ * 如果相交的面积为正，则称两矩形重叠。需要明确的是，只在角或边接触的两个矩形不构成重叠。
+ *
+ * 给出两个矩形，判断它们是否重叠并返回结果。
+ *
+ **/
+public class Ag836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+
+//        rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+
+
+//        y1        y2
+//
+//
+//        x1        x2
+        // 其中一个左边落在另外一个坐标围成的图形内
+
+
+//        x1' < x1 < x2'
+//        y1' < x1 < x2'
+//        x1' < x1 < x2'
+//        x1' < x1 < x2'
+
+
+
+
+
+
+
+
+
+        return true;
+    }
+}
