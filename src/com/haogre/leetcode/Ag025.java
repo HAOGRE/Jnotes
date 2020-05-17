28d9d4d09c3eaf58ea97d63981d22e9db6f69f56
haogre <haogre@gmail.com>
2020-05-17 19:53:28 +0800

    Ag025.java

---
+++ src/com/haogre/leetcode/Ag025.java
@@ -0,0 +1,47 @@
+package com.haogre.leetcode;
+
+import com.haogre.leetcode.extra.ListNode;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Project : Jnotes
+ * @Description: https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+ * @Author : haogre@gmail.com
+ * @Date : 2020/5/16 15:20
+ * @Version : V1.0
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * <p>
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 给你这个链表：1->2->3->4->5
+ * <p>
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ * <p>
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ * <p>
+ *  
+ * <p>
+ * 说明：
+ * <p>
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class Ag025 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+
+
+        return null;
+    }
+}
