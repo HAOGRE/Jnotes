83154313c0096792deb487b19848e998024081f8
haogre <haogre@gmail.com>
2020-05-22 17:10:42 +0800

    Ag343.java

---
+++ src/com/haogre/leetcode/Ag343.java
@@ -0,0 +1,12 @@
+package com.haogre.leetcode;
+
+/**
+ * @Project : Jnotes
+ * @Description: https://leetcode-cn.com/problems/integer-break/solution/343-zheng-shu-chai-fen-tan-xin-by-jyd/
+ * @Author : haogre@gmail.com
+ * @Date : 2020-05-22 17:10
+ * @Version : V1.0
+ */
+public class Ag343 {
+    // TODO 还没刷
+}
