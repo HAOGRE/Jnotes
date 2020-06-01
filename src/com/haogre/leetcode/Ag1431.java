7771b68b25c2bcdc57cf90d1f4f1c44fcf1a0218
haogre <haogre@gmail.com>
2020-06-01 18:12:40 +0800

    Ag1431.java

---
+++ src/com/haogre/leetcode/Ag1431.java
@@ -0,0 +1,12 @@
+package com.haogre.leetcode;
+
+/**
+ * @Project : Jnotes
+ * @Description: https://leetcode-cn.com/problems/kids-with-the-greatest-number-of-candies/
+ * @Author : haogre@gmail.com
+ * @Date : 2020-06-01 18:12
+ * @Version : V1.0
+ */
+public class Ag1431 {
+
+}
