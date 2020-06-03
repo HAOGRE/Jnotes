3e74faef62e178f2d853d2e980cdd1f333fdbd96
haogre <haogre@gmail.com>
2020-06-03 16:50:31 +0800

    1

---
+++ src/com/haogre/leetcode/Ag837.java
@@ -0,0 +1,12 @@
+package com.haogre.leetcode;
+
+/**
+ * @Project : Jnotes
+ * @Description: https://leetcode-cn.com/problems/new-21-game/
+ * @Author : haogre@gmail.com
+ * @Date : 2020-06-03 10:31
+ * @Version : V1.0
+ */
+public class Ag837 {
+
+}
