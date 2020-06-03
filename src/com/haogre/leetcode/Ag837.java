c8a225dc602074f5fc508f82e84a55f6e82b39b0
haogre <haogre@gmail.com>
2020-06-03 16:50:57 +0800

    Ag837.java

--- src/com/haogre/leetcode/Ag837.java
+++ src/com/haogre/leetcode/Ag837.java
@@ -8,5 +8,18 @@ package com.haogre.leetcode;
  * @Version : V1.0
  */
 public class Ag837 {
-
+    public double new21Game(int N, int K, int W) {
+        if (K == 0) {
+            return 1.0;
+        }
+        double[] dp = new double[K + W + 1];
+        for (int i = K; i <= N && i < K + W; i++) {
+            dp[i] = 1.0;
+        }
+        dp[K - 1] = 1.0 * Math.min(N - K + 1, W) / W;
+        for (int i = K - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W;
+        }
+        return dp[0];
+    }
 }
