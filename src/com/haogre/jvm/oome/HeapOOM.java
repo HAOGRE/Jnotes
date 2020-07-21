843c693fdf0939208cbeca25977390462de99c60
haogre <haogre@gmail.com>
2020-07-21 10:21:05 +0800

    add package named jvm

---
+++ src/com/haogre/jvm/oome/HeapOOM.java
@@ -0,0 +1,33 @@
+package com.haogre.jvm.oome;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 如果堆中的对象数量达到最大堆容量限制后就会产生OutOfMemoryError
+ * 这里演示的是java内存区------- 堆内存溢出异常
+ * 
+ * VM args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * 
+ * -XX:+HeapDumpOnOutOfMemoryError 开启快照，如果溢出 Dump出当前存储快照以便事后分析 可使用 Eclipse
+ * Memory Analyzer 来打开堆转储快照文件
+ * 
+ * 逃逸分析，
+ * -XX:+DoEscapeAnalysis -XX:+PrintEscapeAnalysis
+ * 
+ * @author Administrator
+ *
+ */
+public class HeapOOM {
+	static class OOMObject {
+
+	}
+
+	public static void main(String[] args) {
+		List<OOMObject> list = new ArrayList<>();
+		while (true) {
+			list.add(new OOMObject());
+		}
+	}
+
+}
---
+++ src/com/haogre/jvm/oome/JavaVMStackSoF.java
@@ -0,0 +1,29 @@
+package com.haogre.jvm.oome;
+
+/**
+ * 如果线程请求的栈深度大于虚拟机允许的最大深度，就抛出StackOverFlowError
+ * 方法栈
+ * VM args:-Xss128k
+ * 
+ * @author Administrator
+ *
+ */
+public class JavaVmStackSof {
+	private int stackLength = 1;
+
+	public void stackLeak() {
+		stackLength++;
+		// 不断的回调
+		stackLeak();
+	}
+
+	public static void main(String[] args) {
+		JavaVmStackSof oom = new JavaVmStackSof();
+		try {
+			oom.stackLeak();
+		} catch (Exception e) {
+			System.out.println("stack length = " + oom.stackLength);
+			e.printStackTrace();
+		}
+	}
+}
