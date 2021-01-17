7606095ccaa679d12710ccccd0fb35e7385d4ca7
haogre <haogre@gmail.com>
2021-01-18 07:54:48 +0800

    ag721 账户合并 init

--- src/com/haogre/leetcode2021/Ag721.java
+++ src/com/haogre/leetcode2021/Ag721.java
@@ -1,7 +1,15 @@
 package com.haogre.leetcode2021;
 
+import java.util.List;
+
 /**
  * https://leetcode-cn.com/problems/accounts-merge/ 账户合并
  */
 public class Ag721 {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+
+        // 先变为二位数组，然后比较，排列
+
+        return null;
+    }
 }
