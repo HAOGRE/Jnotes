94863eb97c64ad6a0d56f9ead1f829b080bbb8b9
haogre <haogre@gmail.com>
2021-02-19 14:29:11 +0800

    re learn

--- src/com/haogre/leetcode/Ag001.java
+++ src/com/haogre/leetcode/Ag001.java
@@ -19,6 +19,16 @@ public class Ag001 {
 
     }
 
+    public int[] twoSum2021(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
     /**
      * my Solution
      *
