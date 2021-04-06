531cb90256259193f06d91017a12167e3bcfab5c
HAOGRE <haogre@gmail.com>
2021-04-06 20:23:19 +0800

    add ag26 ag80

---
+++ src/com/haogre/leetcode/Ag026.java
@@ -0,0 +1,13 @@
+package com.haogre.leetcode;
+
+/**
+ * @author zhanghao167@meituan.com
+ * @date 2021年04月06日 20:22:00
+ * @Description https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ */
+public class Ag026 {
+    public int removeDuplicates(int[] nums) {
+
+        return 0;
+    }
+}
---
+++ src/com/haogre/leetcode/Ag080.java
@@ -0,0 +1,26 @@
+package com.haogre.leetcode;
+
+/**
+ * @author zhanghao167@meituan.com
+ * @date 2021年04月06日 20:14:00
+ * @Description https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ *  
+ * <p>
+ * 说明：
+ * <p>
+ * 为什么返回数值是整数，但输出的答案是数组呢？
+ * <p>
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ */
+public class Ag080 {
+
+    public int removeDuplicates(int[] nums) {
+
+
+        return 0;
+    }
+}
